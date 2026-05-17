500ed944c36faea36b914c9799741772b52aa502
yarasuharsha <yarasuharsha0@gmail.com>
2026-05-17 10:31:43 +0530

    Time: 20 ms (9.48%), Space: 46 MB (48.23%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap <Character,Integer> freq=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch1=s.charAt(i);
+            if(freq.containsKey(ch1)){
+                freq.put(ch1,freq.get(ch1)+1);
+            }
+            else{
+                freq.put(ch1,1);
+            }
+        }
+        for(int i=0;i<t.length();i++){
+            char ch2=t.charAt(i);
+            if(!freq.containsKey(ch2)){
+                return false;
+            }
+            else{
+                
+                freq.put(ch2,freq.get(ch2)-1);
+                if(freq.get(ch2)==0){
+                    freq.remove(ch2);
+                }
+            }
+        }
+        if(freq.isEmpty()){
+            return true;
+        }
+        return false;
+        
+    }
+}
