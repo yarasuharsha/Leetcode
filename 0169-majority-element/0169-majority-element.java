01e422b863d721461758686d35b404b3318dfac3
yarasuharsha <yarasuharsha0@gmail.com>
2026-07-06 16:25:25 +0530

    Time: 2 ms (70.08%), Space: 56 MB (6.49%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,29 @@
-//Brute Force
+//Optimal Approach Moore's voting 
 class Solution {
     public int majorityElement(int[] nums) {    
+        int count=0;
+        int element=-1;
         for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
+            if(count==0){
+                element=nums[i];
+                count++;
             }
-            if(count>nums.length/2){
-                return nums[i];
+            else if(nums[i]==element){
+                count++;
+            }
+            else{
+                count--;
             }
         }
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==element){
+                c++;
+            }
+        }
+        if(c>nums.length/2){
+            return element;
+        }
         return -1;
-
     }
 }
