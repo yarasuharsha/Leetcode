6e620c695e85e026846a00ac275c96ef068ca49f
yarasuharsha <yarasuharsha0@gmail.com>
2026-08-02 14:14:28 +0530

    Time: 0 ms (100%), Space: 46.2 MB (96.18%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -3,8 +3,6 @@ class Solution {
         int ans[]=new int[2*n];
         for(int i=0;i<n;i++){
             ans[i*2]=nums[i];
-        }
-        for(int i=0;i<n;i++){
             ans[(2*i)+1]=nums[i+n];
         }
         return ans;
